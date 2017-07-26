7d65faec0649c17595944de9300c5445fb371891
Vishal Patil <patilvishalvs@gmail.com>
2017-07-26 13:39:52 +0530

    first commit

--- src/NodeImporter.php
+++ src/NodeImporter.php
@@ -15,22 +15,31 @@ class NodeImporter {
       foreach ($fields_mapper as $key => $field) {
         $field_settings = explode('|', $field['field']);
         $field_name = $field_settings[0];
-        $target_type = isset($field_settings[1])?$field_settings[1]:null;
-        $target_bundle = isset($field_settings[2])?$field_settings[2]:null;
+        $target_type = isset($field_settings[1]) ? $field_settings[1] : null;
+        $target_bundle = isset($field_settings[2]) ? $field_settings[2] : null;
         $settings_count = count($field_settings);
+        $separator = (!empty($field['separator'])) ? $field['separator'] : null;
+        if (!is_null($separator)) {
+          $data[$key] = array_map('trim', explode($separator, $data[$key]));
+        }
+        else {
+          $data[$key] = [$data[$key]];
+        }
         switch ($settings_count) {
           case 1:
             $values[$field_name] = $data[$key];
             break;
           case 2:
-            if($target_type == 'file'){
-              $file_content = file_get_contents($data[$key]);
-              $file = file_save_data($file_content, 'public://file-'.REQUEST_TIME.'.png', FILE_EXISTS_REPLACE);
-              $values[$field_name] = $file->id();
+            if ($target_type == 'file') {
+              foreach ($data[$key] as $path) {
+                $file_content = file_get_contents($path);
+                $file = file_save_data($file_content, 'public://file-' . REQUEST_TIME . '.png', FILE_EXISTS_REPLACE);
+                $values[$field_name][] = $file->id();
+              }
             }
             break;
           case 3:
-            $values[$field_name] = NodeImporter::getTargetId($target_type, $target_bundle, $data[$key], $field['separator']);
+            $values[$field_name] = NodeImporter::getTargetId($target_type, $target_bundle, $data[$key]);
             break;
         }
       }
@@ -52,30 +61,29 @@ class NodeImporter {
     }
     drupal_set_message($message);
   }
-  
-  private static function getTargetId($target_type, $target_bundle, $value, $separator = ','){
-    $values = array_map('trim', explode($separator, $value));
+
+  private static function getTargetId($target_type, $target_bundle, $values) {
     $etids = [];
-    foreach($values as $value){
+    foreach ($values as $value) {
       $properties = [];
       $info = NodeImporter::getTargetInfo($target_type);
       $properties[$info[0]] = $value;
       $properties[$info[1]] = $target_bundle;
       $entity = \Drupal::entityManager()->getStorage($target_type)->loadByProperties($properties);
       $entity = reset($entity);
-      if(empty($entity) && $target_type == 'taxonomy_term'){
+      if (empty($entity) && $target_type == 'taxonomy_term') {
         $entity = Term::create([
-          'name' => $value, 
-          'vid' => $target_bundle,
+              'name' => $value,
+              'vid' => $target_bundle,
         ]);
         $entity->save();
       }
       $etids[] = !empty($entity) ? $entity->id() : 0;
     }
-    return $etids;    
+    return $etids;
   }
-  
-  private static function getTargetInfo($entity_type){
+
+  private static function getTargetInfo($entity_type) {
     $info = [
       'taxonomy_term' => [
         'name',
@@ -88,4 +96,5 @@ class NodeImporter {
     ];
     return $info[$entity_type];
   }
+
 }
